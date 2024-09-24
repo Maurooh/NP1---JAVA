41176fa4d3c869832b4fa1b81a9a9a5af55be398
Maurooh <87792757+Maurooh@users.noreply.github.com>
2024-09-24 15:35:15 -0300

    Caixa.java

---
+++ Caixa.java
@@ -0,0 +1,9 @@
+package revisão;
+
+public interface Caixa {
+	float calculaDesconto();
+}
+
+
+// Uma classe de interface serve para que outras classe usem o mesmo comportamento, mas de jeitos diferentes
+// por exemplo a classe game faz um desconto de 15% e a classe livro faz um desconto de 10%, mas são o mesmo comportamento
